f8015d884ad35619a6f8eee9058b41d04d11a468
patrick brisbin <pbrisbin@gmail.com>
2013-02-10 08:49:55 -0500

    update feature

--- features/step_definitions/user.rb
+++ features/step_definitions/user.rb
@@ -1,3 +1,20 @@
-Given(/^a non-logged in user$/i )do
+Given(/^a non-logged in user$/i) do
   @current_user = nil
+end
+
+Given(/^a logged in user$/i) do
+
+  @current_user = User.create(
+    :email    => 'jim@gmail.com',
+    :password => 'secret'
+  )
+
+  visit '/login'
+
+  fill_in 'email', :with => 'jim@gmail.com'
+  fill_in 'password', :with => 'secret'
+
+  click_button 'Login'
+
+
 end
