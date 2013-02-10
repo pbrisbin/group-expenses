ef269d469c32288cc2ef60693028f98546082203
patrick brisbin <pbrisbin@gmail.com>
2013-02-10 08:49:47 -0500

    add application routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ GrpExp::Application.routes.draw do
 
   match 'about' => 'about#index', :as => :about
 
+  resources :sessions
+
+  match 'login' => 'sessions#new', :as => :login
+  match 'logout' => 'sessions#destroy', :as => :logout
+
+  namespace :user do
+    resource :profile
+    resources :groups
+  end
+
   root :to => 'welcome#index'
 
 end
