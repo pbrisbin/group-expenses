908e9d79124af62cff33da05534b2a2a0ec577df
patrick brisbin <pbrisbin@gmail.com>
2013-02-10 08:47:53 -0500

    setup user session in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :setup_user_session
+
+  attr_accessor :user_session, :current_user
+
+  private
+
+  def setup_user_session
+    @user_session = UserSession.new(session)
+    @current_user = @user_session.user
+  end
+
 end
